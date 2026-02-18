3fce6471f6b0bdd4b9f38aa4274785710d55d276
Ubuntu <ubuntu@ip-172-31-23-183.eu-west-1.compute.internal>
2026-02-18 19:29:09 +0100

    added pep8 compliant script

---
+++ filetest_pep8_compliant.py
@@ -0,0 +1,25 @@
+import pytest
+
+
+def test_calc_addition():
+    """Test du résultat de 2 + 4."""
+    output = 2 + 4
+    assert output == 6
+
+
+def test_calc_subtraction():
+    """Test du résultat de 2 - 4."""
+    output = 2 - 4
+    assert output == -2
+
+
+def test_calc_multiply():
+    """Test du résultat de 2 * 4."""
+    output = 2 * 4
+    assert output == 8
+
+
+def test_coucou():
+    """Test si le résultat renvoie 'hello'."""
+    output = "hello"
+    assert output == "hello"
